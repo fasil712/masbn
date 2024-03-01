d251a5da02f9f43f55e1c763a03436bbd4aae4d8
fasil712 <fasilgetie12@gmail.com>
2024-03-01 23:18:31 +0300

    initial commit

--- src/app/home/main-carousel/main-carousel.component.ts
+++ src/app/home/main-carousel/main-carousel.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main-carousel',
   templateUrl: './main-carousel.component.html',
-  styleUrls: ['./main-carousel.component.scss']
+  styleUrls: ['./main-carousel.component.scss'],
 })
 export class MainCarouselComponent {
-
+  carouselData: any;
 }
