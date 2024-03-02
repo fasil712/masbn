2a56068edf01112db2d69f85bab5fa0ef3fb4d1c
fasil712 <fasilgetie12@gmail.com>
2024-03-02 14:16:30 +0300

    Product Slider added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 
 import { menJeans } from 'src/data/Men/men_jeans';
+import { gounsPage1 } from 'src/data/Gouns/gouns';
+import { lenghacholiPage1 } from 'src/data/Women/LenghaCholi';
+import { kurtaPage1 } from 'src/data/Kurta/kurta';
+import { mensShoesPage1 } from 'src/data/shoes';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,7 +13,16 @@ import { menJeans } from 'src/data/Men/men_jeans';
 })
 export class HomeComponent {
   menJeans: any;
+  womenGouns: any;
+  lenghaChoil: any;
+  mensKurta: any;
+  mensShoes: any;
+
   ngOnInit() {
-    this.menJeans = menJeans.slice(0, 5);
+    this.menJeans = menJeans.slice(0, 4);
+    this.womenGouns = gounsPage1.slice(0, 4);
+    this.lenghaChoil = lenghacholiPage1.slice(0, 4);
+    this.mensKurta = kurtaPage1.slice(0, 4);
+    this.mensShoes = mensShoesPage1.slice(0, 4);
   }
 }
